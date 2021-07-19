994d2f9f88c255987c67531b2841336d90f73862
esiaero <anthias@seas.upenn.edu>
2021-07-19 01:59:49 -0700

    clean repo

--- build/temp.linux-x86_64-3.6/libs/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ponop/pyFlightGoggles/libs/yaml-cpp
-# Build directory: /home/ponop/pyFlightGoggles/build/temp.linux-x86_64-3.6/libs/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
